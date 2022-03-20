f16a3a735501f6874dba77fa41c9b45cd7846466
Keith Sandamele <skeithpatrick@gmail.com>
2022-03-20 14:35:01 +0200

    Update to do list

--- to-do-list.js
+++ to-do-list.js
@@ -2,20 +2,23 @@ var btnAddToDoList = document.getElementById("to-do-list-add-btn");
 var num = 0;
 btnAddToDoList.addEventListener("click", () => {
   num++;
-  var getDate = document.getElementById("to-do-list-due-date").value;
-  var getList = document.getElementById("to-do-list-info").value;
+  var getDate = document.getElementById("to-do-list-due-date");
+  var getList = document.getElementById("to-do-list-info");
+  var setDate = new Date(getDate.value);
   var divDisplayList = document.getElementById("display-list-table-body");
   var combineInfo = `
   <tr id='list${num}'>
-    <td> ${getDate}</td>
-    <td> ${getList}</td> 
+    <td> 
+    ${setDate.getDate()}-${setDate.getMonth()}-${setDate.getFullYear()} ${setDate.getHours()}:${setDate.getMinutes()}
+    </td>
+    <td> ${getList.value}</td> 
     <td>
         <button class='btnDelete' id='delete_${num}' onclick='deleteToDo(this.id)'>X</button>
     </td>
   </tr>`;
   divDisplayList.insertAdjacentHTML("beforeend", combineInfo);
-
-  console.log(num);
+  getDate.value = " ";
+  getList.value = " ";
 });
 
 function deleteToDo(btn_id) {
